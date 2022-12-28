1560c0047ccb62c7de1c29f1c5a0cb127fb524ec
Leonel Huachaca <leonel.huachacav@gmail.com>
2022-12-28 08:57:00 -0500

    add project structure

---
+++ src/app/modules/users/users.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+
+@NgModule({
+  declarations: [FooterComponent, NavbarComponent, InicioComponent, NosotrosComponent, ServiciosComponent, ContactosComponent],
+  imports: [CommonModule],
+})
+export class UsersModule {}
